d410faba3687b4d9df56ae0f06c54ad278f3d19c
nitoo7 <nithinm.10@gmail.com>
2019-11-01 13:46:17 +0530

    Updates

---
+++ precache-manifest.8dc619aba8c42d6e70c61afd05703a1c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cc40c1d4141f4c78c1015481dd4462f",
+    "url": "/my_resume/index.html"
+  },
+  {
+    "revision": "4d4a0199c2ca6073e6af",
+    "url": "/my_resume/static/css/main.aadc8edd.chunk.css"
+  },
+  {
+    "revision": "119f895362dc58d99d94",
+    "url": "/my_resume/static/js/2.a799a67e.chunk.js"
+  },
+  {
+    "revision": "4d4a0199c2ca6073e6af",
+    "url": "/my_resume/static/js/main.f1c88986.chunk.js"
+  },
+  {
+    "revision": "db27e54f2d4f89c685a8",
+    "url": "/my_resume/static/js/runtime-main.c0d170fb.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/my_resume/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "7a9b68c370b8f6ccd26017dd230e5cb4",
+    "url": "/my_resume/static/media/profile_2.7a9b68c3.png"
+  },
+  {
+    "revision": "77e972a1835bd23d6b5d56ecc45dd3d4",
+    "url": "/my_resume/static/media/profile_4.77e972a1.png"
+  }
+]);
